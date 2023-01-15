61584d6d247b1d21c610965488867cf253f5c8ae
Anurup Reddy <koduruanurupreddy2@gmail.com>
2023-01-16 01:20:35 +0530

    defined theme for styled components and created few components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
+import { ThemeProvider } from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Show from './components/pages/Show';
 import Starred from './components/pages/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
-      <Route exact path="/show/:showId">
-        <Show />
-      </Route>
-      <Route>
-        <div>Page Not Found</div>
-      </Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route exact path="/show/:showId">
+          <Show />
+        </Route>
+        <Route>
+          <div>Page Not Found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
