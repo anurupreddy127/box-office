d1fe3a0de582af8ffbb36b1a2b19e218e113e2c0
Anurup Reddy <koduruanurupreddy2@gmail.com>
2023-01-15 15:02:02 +0530

    dreated preview cards for api data

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import MainPage from '../MainPage';
 import { apiGet } from '../../misc/config';
+import ShowGrid from '../show/ShowGrid';
+import ActorGrid from '../actor/ActorGrid';
 
 const Home = () => {
   const [input, setInput] = useState('');
@@ -36,11 +38,11 @@ const Home = () => {
     }
 
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
 
     return null;
